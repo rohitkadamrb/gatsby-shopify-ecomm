6ef77e6204fa6c83d24bd4214805e9ce2e3c2572
rohit <rohitkadamrb@gmail.com>
2020-09-14 18:33:30 +1200

    created slugs for the app

---
+++ src/templates/ProductTemplate/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import { Layout } from 'components';
+import { Grid } from './styles';
+export const query = graphql`
+  query ProductQuery($shopifyId: String) {
+    shopifyProduct(shopifyId: { eq: $shopifyId }) {
+      id
+      title
+    }
+  }
+`;
+
+export default function ProductTemplate(props) {
+  console.log(props);
+  return (
+    <Layout>
+      <Grid>
+        <div>
+          <h1>{props.data.shopifyProduct.title}</h1>
+        </div>
+        <div>Image</div>
+      </Grid>
+    </Layout>
+  );
+}
